610c5c756b91c69158e1d2581d8724368d2f7a4b
sprite_135 <spritesebastian@gmail.com>
2026-05-07 00:46:19 -0500

    Eliminar archivos temporales de backup generados accidentalmente

--- backup.sql
+++
@@ -1,2 +0,0 @@
-Msg 15009, Level 16, State 1, Server DESKTOP-QCGQ9F3, Procedure sp_helptext, Line 54
-The object 'DatabaseContext.InitializeDatabase' does not exist in database 'ScrumSystem' or is invalid for this operation.
